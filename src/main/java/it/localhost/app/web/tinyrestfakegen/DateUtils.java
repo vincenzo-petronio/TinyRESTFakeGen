028054ed66d97d754d14b36640d4645e19fda2f1
vincenzo-petronio <vincenzo.petronio@gmail.com>
2015-06-05 17:17:40 +0200

    FIX DateUtils (forse :D)

--- src/main/java/it/localhost/app/web/tinyrestfakegen/DateUtils.java
+++ src/main/java/it/localhost/app/web/tinyrestfakegen/DateUtils.java
@@ -2,7 +2,10 @@ package it.localhost.app.web.tinyrestfakegen;
 
 import it.localhost.app.web.tinyrestfakegen.exception.DAOException;
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -13,13 +16,18 @@ public class DateUtils {
     
     public static LocalDate getRandomDate() throws DAOException {
         long start = Timestamp.valueOf("1980-01-01 00:00:00").getTime();
-        long end = Timestamp.valueOf("2015-01-01 00:00:00").getTime();
+        long end = Timestamp.valueOf("2015-01-01 00:15:00").getTime();
         long diff = end - start + 1;
-        Timestamp randomTimeStamp = new Timestamp(start + (long)(Math.random() * diff));
+        
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        Instant instant = Instant.ofEpochSecond(start + (long)(Math.random() * diff));
+        LocalDateTime randomDate = LocalDateTime.ofInstant(instant, ZoneId.of("UTC-06:00")); 
+        String r = randomDate.format(format);
         LocalDate date = null;
         try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS", Locale.ITALIAN);
-            date = LocalDate.parse(randomTimeStamp.toString(), formatter);
+//            System.out.println("format: " + r);
+            date = LocalDate.parse(r, format);
+//            System.out.println("parse: " + date.toString());
         } catch (Exception e) {
             throw new DAOException(e.getMessage());
         }
